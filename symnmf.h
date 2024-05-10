4ccb73dbd0a4b4d1e15cc3dd1d549ac44497f23a
gilGree <155623512+gilGree@users.noreply.github.com>
2024-05-10 22:45:54 +0300

    Update symnmf.h

--- symnmf.h
+++ symnmf.h
@@ -4,9 +4,9 @@
 #include <math.h>
 
 /*X is the datapoints*/
-double** sym(double** X);
-double** ddg(double** X);
-double** norm(double** X);
-double** symnmf(double** W,double** H,int k);
+double** sym(double** X,int n,int d);
+double** ddg(double** X,int n,int d);
+double** norm(double** X,int n,int d);
+double** symnmf(double** W,double** H,int n,int k);
 
 #endif
